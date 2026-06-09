c29712cb54a863d04018064ab121f85531015d9a
Vincent Thomas <vincent@v-thomas.com>
2026-06-09 21:59:05 +0200

    update

--- flake.nix
+++ flake.nix
@@ -4,6 +4,10 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    pi-mono = {
+      url = "github:earendil-works/pi";
+      flake = false;
+    };
   };
 
   outputs =
